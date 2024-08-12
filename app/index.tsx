14dbbc6fbb2cd3147c6e24532899d9b9ee2618ca
Coby Keren <137645272+cobykeren@users.noreply.github.com>
2024-08-12 18:19:52 -0400

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+
+export default function Index() {
+  return <PaperProvider></PaperProvider>;
+}
