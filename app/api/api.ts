5c37a6de90e90a774c313995d2a5033c632836d5
Coby Keren <137645272+cobykeren@users.noreply.github.com>
2024-08-30 14:52:33 -0400

    Cleaning code

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,15 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000"; // Use your server's URL in production
-
+const API_BASE_URL = "https://us-central1-cooknook-b5441.cloudfunctions.net";
 export const generateRecipe = async (ingredients: string) => {
   try {
-    const response = await axios.post(
-      `https://us-central1-cooknook-b5441.cloudfunctions.net/generateRecipe`,
-      {
-        ingredients: ingredients,
-      }
-    );
+    const response = await axios.post(`${API_BASE_URL}/generateRecipe`, {
+      ingredients: ingredients,
+    });
 
     return response.data.recipe.content;
   } catch (error) {
--- app/theme.ts
+++ app/theme.ts
@@ -9,15 +9,14 @@ const customTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: "#6200ee", // Primary color (adjust as needed)
-    accent: "#03dac4", // Accent color (adjust as needed)
-    background: "#f6f6f6", // Background color
-    surface: "#ffffff", // Surface color (cards, sheets, etc.)
-    text: "#000000", // Primary text color
-    onBackground: "#000000", // Text color on background
-    onSurface: "#000000", // Text color on surface
-    // Additional colors to override if needed
-    // platform-specific overrides:
+    primary: "#6200ee",
+    accent: "#03dac4",
+    background: "#f6f6f6",
+    surface: "#ffffff",
+    text: "#000000",
+    onBackground: "#000000",
+    onSurface: "#000000",
+
     ...Platform.select({
       ios: {
         primary: "#007AFF", // iOS blue
@@ -26,13 +25,13 @@ const customTheme = {
         primary: "#6200ee", // Android purple
       },
       web: {
-        primary: "#6200ee", // Web purple (same as Android here)
+        primary: "#6200ee",
       },
     }),
   },
-  roundness: 4, // Adjust for iOS-like roundness
+  roundness: 4,
   animation: {
-    scale: 1.0, // Default animation scale
+    scale: 1.0,
   },
 };
 
